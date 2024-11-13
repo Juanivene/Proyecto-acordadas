107f36b5b3171b5550e0fbe304576d58c0fd1f97
Juanivene <168385555+Juanivene@users.noreply.github.com>
2024-11-13 12:43:12 -0300

    feat: header responsive

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,19 +1,26 @@
-import { Box, Stack, Typography } from '@mui/material';
+import { Box, Grid2 as Grid, Typography } from '@mui/material';
 
 const Header = () => (
-  <Stack
+  <Grid
+    container
     direction="row"
-    spacing={2}
+    rowSpacing={2}
     sx={{ justifyContent: 'center', alignItems: 'center' }}
   >
-    <Box
-      component="img"
-      src="https://acordadas.justucuman.gov.ar/img/logos/logo-corte.png"
-      alt="imgCSDJ"
-      sx={{ height: 72 }}
-    />
-    <Typography>Acordadas y Resoluciones</Typography>
-  </Stack>
+    <Grid size={{ xs: 12, md: 6 }} display="flex" justifyContent="center">
+      <Box
+        component="img"
+        src="https://acordadas.justucuman.gov.ar/img/logos/logo-corte.png"
+        alt="imgCSDJ"
+        sx={{ height: 72 }}
+      />
+    </Grid>
+    <Grid size={{ xs: 12, md: 6 }} display="flex" justifyContent="center">
+      <Typography variant="h1" sx={{ fontSize: '2rem' }}>
+        Acordadas y Resoluciones
+      </Typography>
+    </Grid>
+  </Grid>
 );
 
 export default Header;
--- src/index.jsx
+++ src/index.jsx
@@ -4,13 +4,13 @@ import { Provider } from 'react-redux';
 
 import store from './app/store';
 
-import App from './App';
 import './index.css';
+import AgreementView from './View/AgreementView';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <AgreementView />
     </Provider>
   </StrictMode>
 );
