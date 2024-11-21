677bef8132b2c939aff76d0d37ff8b9467706c27
Juanivene <168385555+Juanivene@users.noreply.github.com>
2024-11-21 09:20:45 -0300

    feat: general padding

--- src/Components/Body/Body.jsx
+++ src/Components/Body/Body.jsx
@@ -1,29 +1,35 @@
-import { Grid2 as Grid } from '@mui/material';
+import { Container, Grid2 as Grid } from '@mui/material';
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 
 import Alerts from './Alerts';
 import FormSearchFilter from './Form/FormSearchFilter';
+import TableAgreements from './Table/TableAgreements';
 
 const Body = () => (
-  <Grid
-    container
-    direction="column"
-    spacing={2}
-    sx={{
-      justifyContent: 'center',
-      alignItems: 'center',
-      mt: 3,
-    }}
-  >
-    <Grid size={{ xs: 12 }}>
-      <LocalizationProvider dateAdapter={AdapterDayjs}>
-        <FormSearchFilter />
-      </LocalizationProvider>
+  <Container>
+    <Grid
+      container
+      direction="column"
+      spacing={2}
+      sx={{
+        justifyContent: 'center',
+        alignItems: 'center',
+        mt: 3,
+      }}
+    >
+      <Grid size={{ xs: 12 }}>
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <FormSearchFilter />
+        </LocalizationProvider>
+      </Grid>
+      <Grid size={{ xs: 12 }}>
+        <Alerts />
+      </Grid>
+      <Grid size={{ xs: 12 }}>
+        <TableAgreements />
+      </Grid>
     </Grid>
-    <Grid size={{ xs: 12 }}>
-      <Alerts />
-    </Grid>
-  </Grid>
+  </Container>
 );
 export default Body;
