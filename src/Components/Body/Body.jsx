4b035f3cf1251fa34e7645e20fb65e3e8a3703fe
Juanivene <168385555+Juanivene@users.noreply.github.com>
2024-11-22 12:54:29 -0300

    feat: isErro, show Alert, show Table

--- src/Components/Body/Body.jsx
+++ src/Components/Body/Body.jsx
@@ -1,35 +1,56 @@
+import { useSelector } from 'react-redux';
+
 import { Container, Grid2 as Grid } from '@mui/material';
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 
+import Loader from '../ui/Loader';
 import Alerts from './Alerts';
 import FormSearchFilter from './Form/FormSearchFilter';
+import AlertError from './Table/AlertError';
 import TableAgreements from './Table/TableAgreements';
 
-const Body = () => (
-  <Container>
-    <Grid
-      container
-      direction="column"
-      spacing={2}
-      sx={{
-        justifyContent: 'center',
-        alignItems: 'center',
-        mt: 3,
-      }}
-    >
-      <Grid size={{ xs: 12 }}>
-        <LocalizationProvider dateAdapter={AdapterDayjs}>
-          <FormSearchFilter />
-        </LocalizationProvider>
+const Body = () => {
+  const { dataNow, isLoading, error } = useSelector(
+    (state) => state.getAgreementsFilters
+  );
+  if (isLoading) {
+    <Loader />;
+  }
+  return (
+    <Container>
+      <Grid
+        container
+        direction="column"
+        spacing={2}
+        sx={{
+          justifyContent: 'center',
+          alignItems: 'center',
+          mt: 3,
+        }}
+      >
+        <Grid size={{ xs: 12 }}>
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <FormSearchFilter />
+          </LocalizationProvider>
+        </Grid>
+        <Grid size={{ xs: 12 }}>
+          <Alerts />
+        </Grid>
+        <Grid size={{ xs: 12 }}>
+          {(dataNow && dataNow.data.agreements.length) === 0 || error ? (
+            <AlertError />
+          ) : (
+            ''
+          )}
+          {dataNow && dataNow.data.agreements.length > 0 ? (
+            <TableAgreements />
+          ) : (
+            ''
+          )}
+        </Grid>
       </Grid>
-      <Grid size={{ xs: 12 }}>
-        <Alerts />
-      </Grid>
-      <Grid size={{ xs: 12 }}>
-        <TableAgreements />
-      </Grid>
-    </Grid>
-  </Container>
-);
+    </Container>
+  );
+};
 export default Body;
